d729025abf5601213941da88d63f856dada6d212
Alexandru Grosu <sandry.grosu.24@gmail.com>
2025-03-16 19:00:01 +0100

    Add formula to calculate AC energy

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function searchLocation() {
 }
 
 function updateNumbers(lat, lon) {
-    fetch(`https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=ALLSKY_SFC_SW_DWN&community=SB&longitude=${lon}&latitude=${lat}&start=2023&end=2023`)
+    fetch(`https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=ALLSKY_SFC_SW_DWN&community=RE&longitude=${lon}&latitude=${lat}&start=2023&end=2023`)
         .then(response => response.json())
         .then(data => {
             data = data['properties']['parameter']['ALLSKY_SFC_SW_DWN']
@@ -55,17 +55,33 @@ function updateNumbers(lat, lon) {
             const columnNames = [
                 "January", "February", "March", "April", "May", "June", 
                 "July", "August", "September", "October", "November", "December",
-                "Average",
+                "Annual",
             ];
+
+            const nrOfDays = [
+                31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+            ]
             
             const tableBody = document.querySelector("#dataTable tbody");
             tableBody.innerHTML = "";
+            var sum = 0;
             
             for (const [key, value] of Object.entries(data)) {
                 const index = parseInt(key.slice(4, 6)) - 1; // Extract month part
                 const row = document.createElement("tr");
-                row.innerHTML = `<td>${columnNames[index]}</td><td>${value.toFixed(2)}</td>`;
+                /** 
+                 * We assume the industry standards, where the array size is 20 m2
+                 * The efficiency of a solar panel is 0.18
+                 * The invertor efficiency from DC to AC energy is 0.9
+                 */
+                var AC_energy;
+                if (index < 12) 
+                    AC_energy = value * 20 * 0.18 * 0.9 * nrOfDays[index];
+                else
+                    AC_energy = sum;
+                row.innerHTML = `<td>${columnNames[index]}</td><td>${AC_energy.toFixed(2)}</td>`;
                 tableBody.appendChild(row);
+                sum += AC_energy;
             }
         })
         .catch(error => console.error("Error fetching data:", error));
